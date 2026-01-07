c13ef9a3efc0c8530b1e1e07ee1d2a113b235f90
Alibek <qwereqweqw17@gmail.com>
2026-01-08 01:40:12 +0500

    Update start_daemon.ps1

--- start_daemon.ps1
+++ start_daemon.ps1
@@ -1,8 +1,9 @@
 # PowerShell скрипт для запуска daemon
 # Запуск с правами администратора (рекомендуется)
 
-Write-Host "Запуск Browser Launcher Daemon..." -ForegroundColor Green
-Write-Host "Горячая клавиша: Win + O" -ForegroundColor Yellow
+Write-Host "start Browser Launcher Daemon..." -ForegroundColor Green
+Write-Host "key: Super + O" -ForegroundColor Yellow
 Write-Host ""
 
-python daemon.py
+
+python d0.py
